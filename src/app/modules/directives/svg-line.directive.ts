68bf8984851db4182c9eb42ef136e04da2061347
Andrew KIM <andrewbkim.dev@gmail.com>
2023-06-18 15:23:58 +0900

    add null check logic to <svg-line/> component

--- src/app/modules/directives/svg-line.directive.ts
+++ src/app/modules/directives/svg-line.directive.ts
@@ -11,7 +11,7 @@ import { Line } from '@svgdotjs/svg.js';
 /**
  * Import custom components.
  */
-import { SvgContainerComponent } from '../components/svg-container/svg-container.component';
+import { SvgContainerComponent } from 'app/modules/components';
 
 @Directive({
   selector: 'svg-line'
@@ -20,7 +20,7 @@ export class SvgLineDirective implements AfterViewChecked, OnChanges, OnDestroy 
   /**
    * Globally used variables within the directive.
    */
-  private _line: Line;
+  private _line: Line | null;
 
   /**
    * Import variables for the line directive.
@@ -35,10 +35,10 @@ export class SvgLineDirective implements AfterViewChecked, OnChanges, OnDestroy 
   /**
    * Output variables for the line directive.
    */
-  @Output() clickEvent: EventEmitter<MouseEvent> = new EventEmitter();
-  @Output() doubleClickEvent: EventEmitter<MouseEvent> = new EventEmitter();
-  @Output() mouseOverEvent: EventEmitter<MouseEvent> = new EventEmitter();
-  @Output() mouseOutEvent: EventEmitter<MouseEvent> = new EventEmitter();
+  @Output() clickEvent: EventEmitter<Event> = new EventEmitter();
+  @Output() doubleClickEvent: EventEmitter<Event> = new EventEmitter();
+  @Output() mouseOverEvent: EventEmitter<Event> = new EventEmitter();
+  @Output() mouseOutEvent: EventEmitter<Event> = new EventEmitter();
   @Output() onInitialize: EventEmitter<Line> = new EventEmitter();
 
   /**
@@ -49,7 +49,10 @@ export class SvgLineDirective implements AfterViewChecked, OnChanges, OnDestroy 
   constructor(
     private _svgContainer: SvgContainerComponent,
     private _elRef: ElementRef
-  ) { }
+  ) {
+    this.borderSize = 0;
+    this._line = null;
+  }
 
   /**
    * Creates or updates the line object within the container.
@@ -65,7 +68,7 @@ export class SvgLineDirective implements AfterViewChecked, OnChanges, OnDestroy 
    * Does all required pre-requisites before destroying the component.
    */
   ngOnDestroy(): void {
-    this._line.remove();
+    this._line?.remove();
   }
 
   /**
@@ -77,16 +80,17 @@ export class SvgLineDirective implements AfterViewChecked, OnChanges, OnDestroy 
       // If we have already created the object, update it.
       this.updateLine();
 
+      const { classes } = changes;
       // Check if classes were changed
-      if (changes.classes && changes.classes.currentValue !== changes.classes.previousValue) {
+      if (classes && classes.currentValue !== classes.previousValue) {
         // Get classes that needs to be removed
-        const classesToRemove = changes.classes.previousValue.filter((previousClass: string) =>
-          !changes.classes.currentValue.some((currentClass: string) => currentClass === previousClass)
+        const classesToRemove = classes.previousValue.filter((previousClass: string) =>
+          !classes.currentValue.some((currentClass: string) => currentClass === previousClass)
         );
 
         // Get classes that needs to be added
-        const classesToAdd = changes.classes.currentValue.filter((currentClass: string) =>
-          !changes.classes.previousValue.some((previousClass: string) => currentClass === previousClass)
+        const classesToAdd = classes.currentValue.filter((currentClass: string) =>
+          !classes.previousValue.some((previousClass: string) => currentClass === previousClass)
         );
 
         // Add and remove classes
@@ -99,7 +103,11 @@ export class SvgLineDirective implements AfterViewChecked, OnChanges, OnDestroy 
    * Update line object within the SVG container.
    */
   private updateLine(): void {
-    this._line
+    const line = this._line;
+    if (!line) {
+      return;
+    }
+    line
       .plot(this.x0, this.y0, this.x1, this.y1) // Create the line at specific position
       .stroke({ color: this.borderColor, width: this.borderSize }); // Set the border for the line
 
@@ -111,13 +119,17 @@ export class SvgLineDirective implements AfterViewChecked, OnChanges, OnDestroy 
    * Create line object within the SVG container.
    */
   private createLine(): void {
-    this._line = this._svgContainer.getContainer()
+    const container = this._svgContainer.getContainer();
+    if (!container) {
+      return;
+    }
+    this._line = container
       .line(this.x0, this.y0, this.x1, this.y1) // Create the line at specific position
       .stroke({ color: this.borderColor, width: this.borderSize }) // Set the border for the line
-      .on('click', (evt: MouseEvent) => this.clickEvent.emit(evt)) // Assign click event
-      .on('dblclick', (evt: MouseEvent) => this.doubleClickEvent.emit(evt)) // Assign double click event
-      .on('mouseover', (evt: MouseEvent) => this.mouseOverEvent.emit(evt)) // Assign mouse over event
-      .on('mouseout', (evt: MouseEvent) => this.mouseOutEvent.emit(evt)); // Assign mouse out event
+      .on('click', (evt: Event) => this.clickEvent.emit(evt)) // Assign click event
+      .on('dblclick', (evt: Event) => this.doubleClickEvent.emit(evt)) // Assign double click event
+      .on('mouseover', (evt: Event) => this.mouseOverEvent.emit(evt)) // Assign mouse over event
+      .on('mouseout', (evt: Event) => this.mouseOutEvent.emit(evt)); // Assign mouse out event
 
     // Let's set element in a correct position
     this.setCorrectPosition();
@@ -133,12 +145,18 @@ export class SvgLineDirective implements AfterViewChecked, OnChanges, OnDestroy 
    * Sets correct position for the element.
    */
   private setCorrectPosition() {
+    const container = this._svgContainer.getContainer();
+    const line = this._line;
+    if (!container || !line) {
+      return;
+    }
+
     // Find position of an element within the parent container
     const position = Array.prototype.slice.call(this._elRef.nativeElement.parentElement.children).indexOf(this._elRef.nativeElement);
 
     // Let's update and insert element in a correct position.
-    if (this._svgContainer.getContainer().get(position) && this._line.position() !== position) {
-      this._line.insertBefore(this._svgContainer.getContainer().get(position));
+    if (container.get(position) && line.position() !== position) {
+      line.insertBefore(container.get(position));
     }
   }
 
@@ -151,13 +169,13 @@ export class SvgLineDirective implements AfterViewChecked, OnChanges, OnDestroy 
     // First let's remove classes, that are not necessary anymore
     for (const classToRemove of classesToRemove) {
       this._line
-        .removeClass(classToRemove);
+        ?.removeClass(classToRemove);
     }
 
     // Now let's add new classes
     for (const classToAdd of classesToAdd) {
       this._line
-        .addClass(classToAdd);
+        ?.addClass(classToAdd);
     }
   }
 }
